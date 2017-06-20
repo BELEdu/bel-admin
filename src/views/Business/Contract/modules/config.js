7a192f79d2432b9bfed181207b829043b9895a04
huojinzhao <352782712@qq.com>
2017-06-20 08:50:00 +0800

    合同创建 初始化数据

--- src/views/Business/Contract/modules/config.js
+++ src/views/Business/Contract/modules/config.js
@@ -6,6 +6,65 @@
 
 import { createButton } from '@/utils'
 
+// 编辑字段初始化
+export function editInit() {
+  return {
+    // 合同主要信息
+    info: {
+      // 合同名称：present|required|max:255
+      display_name: '合同名称',
+      // 流程类型编号:present|required
+      flow_id: 1,
+      // 流程中涉及的审批人员编号，按顺序组成如下字符串:present|required
+      auditor_ids: '1,2,3',
+      // 折扣:sometimes|numeric
+      discount: 2,
+      // 辅导地点:sometimes|string|max:255
+      location: '辅导地点',
+      // 审批说明:sometimes|string|max:255
+      note: '审批说明',
+    },
+    // 学员信息
+    student: {
+      // 学员编号，如果有则填写，没有则为null:sometimes|exists:student
+      id: 1,
+      // 学员姓名:present|required|string|max:255
+      display_name: '学员姓名',
+      // 学员联系方式:sometimes|string|size:11
+      phone: '18955555555',
+      // 家长姓名:present|required|string|max:255
+      parent_name: '家长姓名',
+      // 家长联系方式:present|required|string|size:11
+      parent_phone: '18966666666',
+      // 家长身份证号:present|required|string|between:15,18
+      parent_identity_card: '350425111111111111',
+      // 亲属关系:sometimes|string|max:32
+      relation: '父子',
+      // 在读学校:sometimes|string|max:128
+      school: '在读学校',
+      // 学习科目:由于跟学员信息中字段有冲突，暂时不做验证
+      subject_type: '学习科目',
+      // 性别：sometimes|integer
+      gender: 1,
+    },
+    // 产品信息
+    product: [
+      {
+        // 产品编号:required|distinct|exists:products,id
+        product_id: 1,
+        // 购买数量:required|integer|min:1
+        number: 1,
+        // 单品优惠比例:sometimes|numeric|between:0,100
+        discount_rate: 10,
+      },
+      {
+        product_id: 2,
+        number: 1,
+        discount_rate: 10,
+      },
+    ],
+  }
+}
 // eslint-disable-next-line
 export function colConfig(that) {
   return [
