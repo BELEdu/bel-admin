3dded05ef8399d7b343e15f06543dc2de1dfad85
luminghuai <a-thok@outlook.com>
2017-06-26 17:26:06 +0800

    角色与用户管理模块的公共数据转换方法

---
+++ src/views/System/utils.js
@@ -0,0 +1,53 @@
+/**
+ * 递归转换
+ * @author lmh
+ */
+const map = items => items.map(item => ({
+  can_choose: item.can_choose,
+  label: item.display_name,
+  value: item.id || item.role_id,
+  children: item.children ? map(item.children) : [],
+}))
+
+/**
+ * 把后台数据格式转换为兼容cascader组件的格式
+ * @author lmh
+ * @param {Array} data - 需转换的项目
+ */
+export const transform = data => map(data)
+
+/**
+ * 生成每个最深层级项目的id路径数组，以便传递给cascader组件
+ * @author lmh
+ * @param {Array} data - 后台数据
+ */
+export const generatePaths = (data) => {
+  const result = []
+
+  const loop = (items, path) => {
+    items.forEach((item) => {
+      const updatedPath = [...path, item.id || item.role_id]
+      if (item.children) {
+        loop(item.children, updatedPath)
+      } else {
+        result.push(updatedPath)
+      }
+    })
+  }
+
+  loop(data, [])
+
+  return result || []
+}
+
+/**
+ * 根据id获得该id所对应的路径数组，见一个方法
+ * @author lmh
+ * @param {Number} id - 要寻找的id
+ * @param {Array} paths - 所有的路径数组
+ */
+export const getPath = (id, paths) => {
+  const result = paths.find(path => path.includes(id))
+  const index = result.findIndex(pathId => pathId === id)
+  return result.slice(0, index + 1)
+}
