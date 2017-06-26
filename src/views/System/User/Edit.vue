864ea2ab9349da195284d6f939680cec6aa041cc
luminghuai <a-thok@outlook.com>
2017-06-26 17:26:28 +0800

    优化数据的获取，完善部门角色字段的相关操作

--- src/views/System/User/Edit.vue
+++ src/views/System/User/Edit.vue
@@ -34,11 +34,13 @@
       <Form-item label="部门角色">
         <Radio-group v-model="form.default_role_id" style="display: block;">
           <div v-for="role,index in form.roles" class="user-edit-role">
-            <Cascader placeholder="请选择部门与角色" :data="cascaderDepartments"></Cascader>
-            <Radio class="user-edit-role__radio" :label="index">设为默认</Radio>
-            <div class="user-edit-role__btn">
-              <Button type="text" v-show="form.roles.length > 1" @click="form.roles.pop()">删除</Button>
-            </div>
+            <Cascader
+              placeholder="请选择部门与角色" :data="roles" v-model="form.roles[index]"
+              @on-change="(value, detail) => selectRole(value, detail, index)"
+            ></Cascader>
+            <Button class="user-edit-role__modify" :class="{hidden: !isRoleInDb(role)}" type="text">修改权限</Button>
+            <Radio class="user-edit-role__radio" :class="{hidden: !role.length}" :label="role[role.length - 1]">设为默认</Radio>
+            <Button :class="{hidden: form.roles.length <= 1}" type="text" @click="form.roles.pop()">删除</Button>
           </div>
         </Radio-group>
         <Button type="dashed" icon="plus" class="color-primary" size="large" @click="form.roles.push([])">
@@ -59,7 +61,7 @@
       </Form-item>
 
       <Form-item>
-        <Button type="ghost" size="large">取消</Button>
+        <Button type="ghost" size="large" @click="goBack">取消</Button>
         <Button type="primary" size="large" @click="submit">提交</Button>
       </Form-item>
     </Form>
@@ -73,14 +75,21 @@
  * @version 2017-06-19
  */
 
-import { mapState, mapGetters } from 'vuex'
-import { GLOBAL, SYSTEM } from '@/store/mutationTypes'
+import { mapState } from 'vuex'
+import { GLOBAL } from '@/store/mutationTypes'
+import { transform, generatePaths, getPath } from '../utils'
 
 export default {
   name: 'app-system-user-detail',
 
   data() {
     return {
+      roles: [],
+      rolePaths: [],
+
+      // 后台返回的该用户已有的角色，只有这些角色可以被修改权限
+      rolesInDb: [],
+
       form: {
         username: '',
         realname: '',
@@ -137,7 +146,6 @@ export default {
       genders: state => state.dicts.gender,
       jobTypes: state => state.dicts.users_job_type,
     }),
-    ...mapGetters(['cascaderDepartments']),
 
     id() {
       return this.$router.currentRoute.params.id
@@ -145,27 +153,81 @@ export default {
   },
 
   methods: {
+    // 获取部门角色列表
+    getData() {
+      return this.$http.get('/department_role')
+        .then((res) => {
+          this.roles = transform(res)
+          this.rolePaths = generatePaths(res)
+        })
+    },
+
+    // 获取当前用户信息
     getUser() {
       this.$http.get(`/user/${this.id}`)
         .then((res) => {
           this.form = {
             ...res,
+            roles: res.roles.map(role => getPath(role.id, this.rolePaths)),
             status: !!res.status,
           }
+          this.rolesInDb = [...this.form.roles]
         })
+    },
+
+    isRoleInDb(role) {
+      return this.rolesInDb.some(roleInDb => String(roleInDb) === String(role))
+    },
+
+    selectRole(value, detail, index) {
+      const target = detail[detail.length - 1]
+
+      const canNotChoose = target.can_choose === 0 // 该项是部门而非角色
+      const hasSelected = this.form.roles // 该项已被选中过
+        .slice(0, -1)
+        .some(role => role[role.length - 1] === target.value)
+
+      if (canNotChoose || hasSelected) {
+        this.form = {
+          ...this.form,
+          roles: [
+            ...this.form.roles.slice(0, index),
+            [],
+            ...this.form.roles.slice(index + 1),
+          ],
+        }
+      }
+
+      if (canNotChoose) {
+        this.$Message.error('该部门下暂无角色')
+      }
+      if (hasSelected) {
+        this.$Message.error('请勿重复选择角色')
+      }
     },
 
     submit() {
       this.$refs.form.validate((valid) => {
         if (valid) {
-          this.$http.post('/user', this.form)
+          const data = {
+            ...this.form,
+            status: +this.form.status,
+          }
+          const request = this.id ? () => {} : this.$http.post('/user', data)
+
+          request
+            .then(() => this.goBack())
         }
       })
+    },
+
+    goBack() {
+      this.$router.go(-1)
     },
   },
 
   created() {
-    this.$store.dispatch(SYSTEM.DEPARTMENT.INIT)
+    this.getData()
       .then(() => {
         if (this.id) this.getUser()
       })
@@ -179,20 +241,32 @@ export default {
 <style lang="less">
 .user-edit-role {
   display: flex;
+  align-items: center;
   margin-bottom: 20px;
 
   .ivu-cascader {
     flex: 1;
   }
 
-  &__radio {
+  &__radio.ivu-radio-wrapper {
     flex: 0;
-    margin: 0 20px;
+    margin: 0 10px 0 25px;
+    font-size: 12px !important;
   }
 
   &__btn {
     flex: none;
     width: 56px;
+  }
+
+  &__modify {
+    position: relative;
+    margin-left: -90px;
+  }
+
+  .hidden {
+    opacity: 0;
+    visibility: hidden;
   }
 }
 </style>
