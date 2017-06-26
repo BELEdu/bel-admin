17a067514668d828db2582d62b0e8e829913cbea
luminghuai <a-thok@outlook.com>
2017-06-26 17:11:17 +0800

    编辑页接口不应经由store派发

--- src/store/system/role.js
+++ src/store/system/role.js
@@ -6,16 +6,11 @@ export default {
     list: {
       data: [],
     },
-    permis: {},
   },
 
   mutations: {
     [SYSTEM.ROLE.INIT](state, list) {
       state.list = list
-    },
-
-    [SYSTEM.ROLE.PERMIS.INIT](state, permis) {
-      state.permis = permis
     },
   },
 
@@ -23,11 +18,6 @@ export default {
     [SYSTEM.ROLE.INIT]({ commit }, query = '') {
       return Http.get(`/role${query}`)
         .then(res => commit(SYSTEM.ROLE.INIT, res))
-    },
-
-    [SYSTEM.ROLE.PERMIS.INIT]({ commit }) {
-      return Http.get('/role/create')
-        .then(res => commit(SYSTEM.ROLE.PERMIS.INIT, res))
     },
   },
 }
