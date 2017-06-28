8a2cea310c581824bb615aac2b381cae48adb051
zhoumenglin <512648622@qq.com>
2017-06-28 11:38:01 +0800

    学员管理：交流会上传附件

--- src/views/Student/Student/Detail/Meeting/Edit.vue
+++ src/views/Student/Student/Detail/Meeting/Edit.vue
@@ -19,10 +19,8 @@
         <Input placeholder="请输入家长姓名，多人以 “，” 分隔" v-model="form.parent_name"></Input>
       </Form-item>
       <Form-item label="参会员工">
-        <Select placeholder="请选择..." v-model="form.meeting_person" multiple filterable>
-          <Option value="1">张三</Option>
-          <Option value="2">李四</Option>
-          <Option value="3">王五</Option>
+        <Select placeholder="请选择..." v-model="form.meeting_persons" multiple filterable>
+          <Option v-for="item in meeting_persons_data" :value="item.value" :key="item.display_name">{{ item.display_name }}</Option>
         </Select>
       </Form-item>
       <Form-item label="会议类型" prop="meeting_type">
@@ -30,26 +28,43 @@
           <Option v-for="type in meetingTypes" :key="type.value" :value="type.value">{{ type.display_name }}</Option>
         </Select>
       </Form-item>
-      <Form-item label="学科水平" >
-        <Input type="textarea" v-model="form.content_one" :autosize="{minRows: 4,maxRows: 8}" placeholder="学员基本信息及学科水平介绍（咨询师）"></Input>
-      </Form-item>
-      <Form-item label="沟通交流情况" >
-        <Input type="textarea" v-model="form.content_two" :autosize="{minRows: 4,maxRows: 8}" placeholder="沟通交流（学管师）"></Input>
-      </Form-item>
-      <Form-item label="备课建议" >
-        <Input type="textarea" v-model="form.content_three" :autosize="{minRows: 4,maxRows: 8}" placeholder="第一次备课、上课的注意事项及建议"></Input>
-      </Form-item>
+
+      <div v-if="form.meeting_type === 1">
+        <Form-item label="学科水平" >
+          <Input type="textarea" v-model="form.meeting_content[0].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="学员基本信息及学科水平介绍（咨询师）"></Input>
+        </Form-item>
+        <Form-item label="沟通交流情况" >
+          <Input type="textarea" v-model="form.meeting_content[1].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="沟通交流（学管师）"></Input>
+        </Form-item>
+        <Form-item label="备课建议" >
+          <Input type="textarea" v-model="form.meeting_content[2].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="第一次备课、上课的注意事项及建议"></Input>
+        </Form-item>
+      </div>
+
+      <div v-if="form.meeting_type === 3">
+        <Form-item label="近期学情" >
+          <Input type="textarea" v-model="form.meeting_content[3].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="近期学情反馈"></Input>
+        </Form-item>
+        <Form-item label="需要提升的地方" >
+          <Input type="textarea" v-model="form.meeting_content[4].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="需要提升的地方"></Input>
+        </Form-item>
+        <Form-item label="家长意见与要求" >
+          <Input type="textarea" v-model="form.meeting_content[5].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="家长意见与要求"></Input>
+        </Form-item>
+        <Form-item label="本次会议结果" >
+          <Input type="textarea" v-model="form.meeting_content[6].content" :autosize="{minRows: 4,maxRows: 8}" placeholder="本次会议结果"></Input>
+        </Form-item>
+      </div>
+
       <Form-item  >
-        <Upload
-        multiple
-        type="drag"
-        action="//jsonplaceholder.typicode.com/posts/"
-        >
-          <div style="padding: 20px 0">
-            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-            <p>点击或将文件拖拽到这里上传</p>
-          </div>
-        </Upload>
+        <app-uploader
+          action="/meeting/upload"
+          name="meeting_file"
+          @on-success="uploadSuccess"
+          @on-error="uploadError"
+          @on-remove="uploadRemove"
+          :files="files"
+        ></app-uploader>
       </Form-item>
       <Form-item>
         <Button @click="goBack()">取消</Button>
@@ -67,6 +82,7 @@
  * @author zml
  * @version 2017-06-14
  */
+
 import { mapState } from 'vuex'
 import { GLOBAL } from '@/store/mutationTypes'
 import format from 'date-fns/format'
@@ -80,15 +96,43 @@ export default {
         id: '',
         student_id: '',
         meeting_date: '',
-        meeting_type: '',
+        meeting_type: null,
         parent_name: '',
-        content_one: '',
-        content_two: '',
-        content_three: '',
-        content_four: '',
-        meeting_person: [],
+        meeting_content: [
+          {
+            content_tag: '学科水平',
+            content: '',
+          },
+          {
+            content_tag: '沟通交流情况',
+            content: '',
+          },
+          {
+            content_tag: '备课建议',
+            content: '',
+          },
+          {
+            content_tag: '近期学情',
+            content: '',
+          },
+          {
+            content_tag: '需要提升的地方',
+            content: '',
+          },
+          {
+            content_tag: '家长意见与要求',
+            content: '',
+          },
+          {
+            content_tag: '本次会议结果',
+            content: '',
+          },
+        ],
+        meeting_persons: [],
         meeting_attachment: [],
       },
+
+      meeting_persons_data: [],
 
       rules: {
         meeting_date: [
@@ -121,6 +165,13 @@ export default {
     isUpdate() {
       return !!this.$router.currentRoute.params.meetingId
     },
+    files() {
+      return this.form.meeting_attachment
+        .map(({ name, url }) => ({
+          name,
+          url,
+        }))
+    },
   },
 
   methods: {
@@ -136,11 +187,43 @@ export default {
     getClassData() {
       return this.$http.get(`/meeting/${this.meetingId}`)
         .then((res) => {
+          const {
+            meeting_persons_data,
+            ...form
+          } = res
+
           this.form = {
-            ...res,
+            ...form,
             meeting_date: new Date(res.meeting_date),
           }
+          this.meeting_persons_data = meeting_persons_data
         })
+    },
+
+    // 图片上传成功
+    uploadSuccess(res) {
+      this.form = {
+        ...this.form,
+        meeting_attachment: [
+          ...this.form.meeting_attachment,
+          res,
+        ],
+      }
+    },
+
+    // 图片上传失败
+    uploadError() {
+      // 这里可以的话，应该进一步细腻化，按实际的错误显示错误消息
+      this.$Message.error('图片上传失败')
+    },
+
+    // 从已上传的图片列表中删除一张图片
+    uploadRemove(targetFile) {
+      this.form = {
+        ...this.form,
+        meeting_attachment: this.form.meeting_attachment
+          .filter(file => file.url !== targetFile.url),
+      }
     },
 
      // 提交表单
