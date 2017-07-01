d4d2d0305ff075ac8a624bdf6f74ea18fab1bac4
zhoumenglin <512648622@qq.com>
2017-07-01 17:19:48 +0800

    公共校验规则更新

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -41,6 +41,20 @@ const rules = {
     trigger: 'blur',
   }),
 
+  // 数字验证
+  number: {
+    pattern: /^[0-9]*$/,
+    message: '请输入数字',
+    trigger: 'blur',
+  },
+
+  // 价格验证(小数点后最多两位，不能以01开头，小数点后必须要有数字)
+  price: {
+    pattern: /^(0|[1-9][0-9]{0,9})(\.[0-9]{1,2})?$/,
+    message: '请输入正确的价格',
+    trigger: 'blur',
+  },
+
   // 手机号码验证
   mobile: {
     pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|17[0135678]|18[0-9])\d{8}$/,
@@ -48,10 +62,31 @@ const rules = {
     trigger: 'blur',
   },
 
+  // 身份证验证
+  idcard: {
+    pattern: /^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/,
+    message: '身份证格式不正确',
+    trigger: 'blur',
+  },
+
   // 邮箱验证
   email: {
     type: 'email',
     message: '邮箱地址格式不正确',
+    trigger: 'blur',
+  },
+
+  // 汉字验证
+  chinese: {
+    pattern: /^[\u4e00-\u9fa5]{0,}$/,
+    message: '请输入汉字',
+    trigger: 'blur',
+  },
+
+  // 姓名验证（中文包含少数民族如：买买提·陈奕迅·阿凡提，英文名支持由空格和·分隔开）
+  name: {
+    pattern: /^([\u4e00-\u9fa5]+|[\u4e00-\u9fa5]+(·[\u4e00-\u9fa5]+)*|([a-zA-Z]+\s?)+|[a-zA-Z]+(·[a-zA-Z]+)*)$/,
+    message: '姓名格式不正确',
     trigger: 'blur',
   },
 }
