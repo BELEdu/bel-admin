f0b6dec039ec727be5f47f0f1abd87e10139c55d
chenliangshan <729755202@qq.com>
2017-07-03 19:56:02 +0800

    修改周课表组件

--- src/views/Curriculum/Components/WeeklyTable.vue
+++ src/views/Curriculum/Components/WeeklyTable.vue
@@ -2,33 +2,42 @@
   <div class="weekly-table">
     <div class="weekly-left">
       <div class="info">
-        <p>
-          <label>学员姓名：</label>
-          <span>李佳航</span>
-        </p>
+        <template v-if="isClass">
+          <p>
+            <label>班级名称：</label>
+            <span>{{currentDate.model_info.display_name}}</span>
+          </p>
+        </template>
+        <template v-else>
+          <p>
+            <label>学员姓名：</label>
+            <span>{{currentDate.model_info.display_name}}</span>
+          </p>
+        </template>
+
         <p>
           <label>上课年级：</label>
-          <span>初中三年级</span>
+          <span><app-dicts-filter :value="currentDate.grade" name="grade"></app-dicts-filter></span>
         </p>
         <p>
           <label>产品名称：</label>
-          <span>个性化一对一</span>
+          <span>{{currentDate.product_name}}</span>
         </p>
         <p>
           <label>教师姓名：</label>
-          <span>张旭</span>
+          <span>{{currentDate.teacher_name}}</span>
         </p>
         <p>
           <label>上课科目：</label>
-          <span>数学</span>
+          <span>{{currentDate.subject_type}}</span>
         </p>
         <p>
           <label>计划课时：</label>
-          <span>2</span>
+          <span>{{currentDate.course_cost}}</span>
         </p>
         <p>
           <label>实际课时：</label>
-          <span>1</span>
+          <span>{{currentDate.fact_cost}}</span>
         </p>
       </div>
       <div class="switch">
@@ -69,125 +78,52 @@
       <div class="app-table ivu-table-wrapper">
         <div class="ivu-table ivu-table-border">
           <div class="ivu-table-header">
-            <!--<table cellspacing="0" cellpadding="0" border="0" width="100%">
-              <colgroup>
-                <col width="50">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-              </colgroup>
-              <thead>
-              <tr>
-                <th class="ivu-table-column-center">
-                  <div class="ivu-table-cell">
-                    <span></span>
-                  </div>
-                </th>
-                <th class="ivu-table-column-center" v-for="thead in weeklyData.week">
-                  <div class="ivu-table-cell">
-                    <span>{{thead.date}}</span>
-                  </div>
-                </th>
-              </tr>
-              </thead>
-            </table>-->
             <ul class="weekly-ul-head clearfix">
               <li></li>
               <li v-for="thead in weeklyData.week">{{thead.date}}</li>
             </ul>
           </div>
           <div class="ivu-table-body" v-if="weeklyData.week">
-            <!--<table cellspacing="0" cellpadding="0" border="0" width="100%">
-              <colgroup>
-                <col width="50">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-                <col width="14%">
-              </colgroup>
-              <tbody class="ivu-table-tbody">
-                <tr class="ivu-table-row" v-for="(list,index) in weeklyData">
-                  <template v-for="(item,key) in list">
-                    <template v-if="index%4==0&&key==0">
-                      <td class="ivu-table-column-center" rowspan="4">
-                        <div class="ivu-table-cell">
-                      <span>
-                        <template v-if="!index">
-                          上午
-                        </template>
-                        <template v-if="index == 4">
-                          下午
-                        </template>
-                        <template v-if="index == 8">
-                          晚上
-                        </template>
-                      </span>
-                        </div>
-                      </td>
-                    </template>
-                    <td class="ivu-table-column-center" @click="courseDetails(item,index,key)" v-bind:class="[{ 'weekly-active': isActive==item.id }, {'weekly-course': item.subjects_class}, weakStatus(item)]">
-                      <div class="ivu-table-cell">
-                        <span>
-                          <template v-if="item.subjects_class">
-                            <Row>
-                            <Col span="24">{{item.class_time}}</Col>
+            <ul class="weekly-ul clearfix">
+              <template v-for="(list, key) in weeklyData.week">
+                <li class="li-head" v-if="key == 0">
+                  <div class="morning" :style="{height: morningHight}">
+                    <span>上午</span>
+                  </div>
+                  <div class="afternoon" :style="{height: afternoonHight}">
+                    <span>下午</span>
+                  </div>
+                  <div class="evening" :style="{height: eveningHight}">
+                    <span>晚上</span>
+                  </div>
+                </li>
+                <li class="weekly-li">
+                  <template v-for="(v, k) in weeklyData.count">
+                    <div :class="k" :ref="k">
+                      <div class="weekly-cost" v-for="(item,index) in list.course[k]" @click="courseDetails(item)" v-bind:class="[{ 'weekly-active': isActive==item.id }, weakStatus(item)]">
+                        <div class="weekly-course">
+                          <Row>
+                            <Col span="24">
+                            {{item.start_at}}-{{item.end_at}}</Col>
                           </Row>
                           <Row>
-                            <Col span="12">{{item.subjects_class}}</Col>
-                            <Col span="12">{{item.student_name}}</Col>
+                            <Col span="12">
+                            {{item.subject_type}}
+                            </Col>
+                            <Col span="12">
+                            {{item.teacher_name}}</Col>
                           </Row>
-                          </template>
-                          <template v-else>
-                            <Icon type="plus-round" v-bind:class="'weekly-icon'"></Icon>
-                          </template>
-                        </span>
+                        </div>
                       </div>
-                    </td>
-                  </template>
-                </tr>
-              </tbody>
-            </table>-->
-            <ul class="weekly-ul clearfix">
-              <li>
-                <div>
-                  上午
-                </div>
-                <div>
-                  下午
-                </div>
-                <div>
-                  晚上
-                </div>
-              </li>
-              <li class="weekly-li" v-for="list in weeklyData.week">
-                <template v-for="(v, k) in weeklyData.count">
-                  <div v-for="item in list.course[k]">
-                    <Row>
-                      <Col span="24">
-                      {{item.start_at}}-{{item.end_at}}</Col>
-                    </Row>
-                    <Row>
-                      <Col span="12">
-                      <app-dicts-filter :value="item.grade" name="grade"></app-dicts-filter>
-                      </Col>
-                      <Col span="12">
-                      {{item.teacher_name}}</Col>
-                    </Row>
-                  </div>
-                  <template v-if="v-list.course[k].length>0">
-                    <div v-for="item in (v-list.course[k].length)">
-                      +
+                      <template v-if="v-list.course[k].length>0">
+                        <div class="weekly-cost" v-for="item in (v-list.course[k].length)">
+                          <span class="weekly-icon">+</span>
+                        </div>
+                      </template>
                     </div>
                   </template>
-                </template>
-              </li>
+                </li>
+              </template>
             </ul>
           </div>
           <div class="ivu-table-tip" v-show="!weeklyData.week || !weeklyData.week.length">
@@ -206,6 +142,9 @@
 </template>
 
 <script>
+  /**
+   * 周课表
+   */
 
   export default{
     name: 'weekly-table',
@@ -218,31 +157,39 @@
     data() {
       return {
         isActive: '',
+        morningHight: '',
+        afternoonHight: '',
+        eveningHight: '',
+        currentDate: {},
       }
     },
     computed: {
       weeklyData() {
         return this.data
       },
+      isClass() {
+        const pathArry = this.$route.path.split('/')
+        return pathArry[pathArry.length - 4] === 'teachercurricula' || pathArry[pathArry.length - 4] === 'class'
+      },
     },
     methods: {
       courseDetails(item) {
+        this.currentDate = item
         this.isActive = item.id
-        window.console.log(item)
       },
       weakStatus(item) {
         let className
-        switch (item.status_class) {
-          case '1':
-            className = 'finish'
-            break
-          case '2':
-            className = 'verify'
-            break
-          case '3':
+        switch (item.schedule_status) {
+          case 0:
             className = 'confirm'
             break
-          case '4':
+          case 1:
+            className = 'verify'
+            break
+          case 2:
+            className = 'finish'
+            break
+          case 3:
             className = 'cancel'
             break
           default :
@@ -251,16 +198,51 @@
         }
         return className
       },
+      // 设置默认展示
+      getCurrentDate() {
+        const data = this.data.week.map((item) => {
+          const course = item.course
+          if (course) {
+            if (course.morning.length) {
+              return course.morning[0]
+            } else if (course.afternoon.length) {
+              return course.afternoon[0]
+            } else if (course.evening.length) {
+              return course.evening[0]
+            }
+          }
+          return []
+        })
+        if (data.length) {
+          this.currentDate = data[0]
+          this.isActive = data[0].id
+        }
+      },
+      // 设置标签高度
+      getRef() {
+        this.$nextTick(() => {
+          this.morningHight = `${this.$refs.morning[0].offsetHeight}px`
+          this.afternoonHight = `${this.$refs.afternoon[0].offsetHeight}px`
+          this.eveningHight = `${this.$refs.evening[0].offsetHeight}px`
+        })
+      },
+    },
+    watch: {
+      data() {
+        this.getCurrentDate()
+        this.getRef()
+      },
     },
   }
 </script>
 
 <style lang="less">
   @import "~vars";
-  @finish       : #66bae5;
-  @verify       : #e5bb79;
-  @confirm      : #e47fa9;
-  @cancel       : #ccc;
+
+  @finish: #66bae5;
+  @verify: #e5bb79;
+  @confirm: #e47fa9;
+  @cancel: #ccc;
 
   .weekly-table {
     position: relative;
@@ -282,13 +264,13 @@
           label {
             width: 76px;
             position: absolute;
-            left:0;
+            left: 0;
             line-height: 30px;
             text-align: right;
           }
           span {
             display: inline-block;
-            padding-top: 4px;
+            padding-top: 6px;
             line-height: 20px;
           }
         }
@@ -305,7 +287,7 @@
           &.prev {
             border-left: 1px solid #d7dde4;
           }
-          &.prev,&.next {
+          &.prev, &.next {
             color: @primary-color;
             cursor: pointer;
           }
@@ -352,43 +334,76 @@
           &:first-child {
             width: 4%;
           }
-          > div {
-            height: 40px;
-          }
         }
       }
       .weekly-ul-head {
+        border-bottom: 1px solid #d7dde4;
         li {
           font-weight: bold;
+          line-height: 40px;
+          border-left: 1px solid #d7dde4;
+          &:first-child {
+            border: 0;
+          }
         }
       }
-      .ivu-table,.ivu-table-body {
+      .weekly-ul {
+        li {
+          border-left: 1px solid #d7dde4;
+          &.li-head {
+            border: 0;
+            > div {
+              border-bottom: 1px solid #d7dde4;
+              position: relative;
+              span {
+                width: 20px;
+                display: block;
+                line-height: 50px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                margin-top: -50px;
+                margin-left: -10px;
+              }
+            }
+          }
+          .weekly-cost {
+            height: 58px;
+            padding: 10px 0;
+            border-bottom: 1px solid #d7dde4;
+            box-sizing: border-box;
+            &.finish {
+              background-color: @finish;
+            }
+            &.verify {
+              background-color: @verify;
+            }
+            &.confirm {
+              background-color: @confirm;
+            }
+            &.cancel {
+              background-color: @cancel;
+            }
+            .weekly-course {
+              color: #fff;
+            }
+          }
+        }
+      }
+      .ivu-table, .ivu-table-body {
         overflow: visible;
       }
       .ivu-table-cell {
         border: 1px solid transparent;
       }
       .weekly-icon {
-        color: #ccc
-      }
-      .weekly-course {
-        color: #fff;
-        &.finish {
-          background-color: @finish;
-        }
-        &.verify {
-          background-color: @verify;
-        }
-        &.confirm {
-          background-color: @confirm;
-        }
-        &.cancel {
-          background-color: @cancel;
-        }
+        color: #ccc;
+        line-height: 40px;
+        font-size: 20px;
       }
       .weekly-active {
         position: relative;
-        .ivu-table-cell {
+        .weekly-course {
           width: 110%;
           position: absolute;
           padding: 14px 0;
@@ -399,22 +414,22 @@
           z-index: 10;
         }
         &.finish {
-          .ivu-table-cell {
+          .weekly-course {
             background-color: @finish;
           }
         }
         &.verify {
-          .ivu-table-cell {
+          .weekly-course {
             background-color: @verify;
           }
         }
         &.confirm {
-          .ivu-table-cell {
+          .weekly-course {
             background-color: @confirm;
           }
         }
         &.cancel {
-          .ivu-table-cell {
+          .weekly-course{
             background-color: @cancel;
           }
         }
