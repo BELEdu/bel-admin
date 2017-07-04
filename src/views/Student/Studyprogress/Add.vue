c69b6342234333b9eefcbeb541f47a604312e454
zhoumenglin <512648622@qq.com>
2017-07-04 17:12:25 +0800

    学员管理：学习进度，添加计划模块

---
+++ src/views/Student/Studyprogress/Add.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <app-editor-title></app-editor-title>
+    <edit-plan></edit-plan>
+  </div>
+</template>
+
+<script>
+/**
+ * 学员管理 - 学习进度 - 添加计划
+ * @author zml
+ * @version 2017-07-04
+ */
+
+import { GLOBAL } from '@/store/mutationTypes'
+import EditPlan from './components/EditPlan'
+
+export default {
+  name: 'app-student-studyprogress-add',
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    type() {
+      return this.$router.currentRoute.params.type
+    },
+  },
+
+  components: {
+    EditPlan,
+  },
+
+  created() {
+    this.$store.commit(GLOBAL.LOADING.HIDE)
+    this.$route.meta.breadcrumb[1].link = `/student/studyprogress/${this.type}`
+  },
+}
+</script>
+
+<style lang="less">
+@import "~vars.less";
+</style>
