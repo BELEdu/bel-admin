a2f017b9e2dd1ce7d23a8e180f571c81d47c7e2d
luminghuai <a-thok@outlook.com>
2017-08-01 17:24:18 +0800

    改进代码质量

--- src/mixins/broadcast.js
+++ src/mixins/broadcast.js
@@ -3,13 +3,27 @@
  * @author  chenliangshan
  * @version 2017/08/01
  */
-import { broadcast } from '@/utils'
+
+/**
+  * @param {String} componentName - 目标组件的name属性
+  、@param {String} eventName - 广播事件名称
+  * @param {Array} params - 广播事件参数
+  */
+function broadcast(componentName, eventName, ...params) {
+  this.$children.forEach((child) => {
+    if (child.$options.name === componentName) {
+      child.$emit(eventName, ...params)
+    } else {
+      broadcast.call(child, componentName, eventName, ...params)
+    }
+  })
+}
 
 export default {
   methods: {
     // 事件广播
-    broadcast(componentName, eventName, params) {
-      broadcast.call(this, componentName, eventName, params)
+    broadcast(...args) {
+      broadcast.call(this, ...args)
     },
   },
 }
