5797cc0551a47551cafdc28f1d0895184eb55a9e
huojinzhao <352782712@qq.com>
2017-09-10 17:18:54 +0800

    feat(Question): 知识点批量编辑页，静态

--- src/views/Question/Knowledge/Edition.vue
+++ src/views/Question/Knowledge/Edition.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>知识点批量编辑</div>
+  <div class="question-knowledge-edition">
+    <aside>
+      <v-side-choosen></v-side-choosen>
+    </aside>
+    <section>
+      <div class="edition-item" v-for="i in 10" :key="i">
+        <h3>
+          知识点名称:
+          <span>平行线</span>
+          <i class="ivu-icon ivu-icon-android-delete"></i>
+        </h3>
+        <Form :label-width="80" inline>
+          <Form-item
+            label="理科频次"
+          >
+            <InputNumber></InputNumber>
+          </Form-item>
+          <Form-item
+            label="理科分值"
+          >
+            <InputNumber></InputNumber>
+          </Form-item>
+          <Form-item
+            label="文科频次"
+          >
+            <InputNumber></InputNumber>
+          </Form-item>
+          <Form-item
+            label="文科分值"
+          >
+            <InputNumber></InputNumber>
+          </Form-item>
+          <!-- <Form-item
+            label="考试频次"
+          >
+            <InputNumber></InputNumber>
+          </Form-item>
+          <Form-item
+            label="考试分值"
+          >
+            <InputNumber></InputNumber>
+          </Form-item> -->
+        </Form>
+      </div>
+    </section>
+    <footer>
+      <Button
+        type="ghost"
+        @click="cancel"
+      >取消</Button>
+      <Button
+        type="primary"
+        @click="confirm"
+      >确认</Button>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -10,9 +65,24 @@
  */
 
 import { GLOBAL } from '@/store/mutationTypes'
+import vSideChoosen from '../components/SideChoosen'
 
 export default {
   name: 'question-knowledge-edition',
+
+  components: {
+    vSideChoosen,
+  },
+
+  methods: {
+    cancel() {
+      this.$router.push('/question/knowledge')
+    },
+
+    confirm() {
+      //
+    },
+  },
 
   created() {
     this.$store.commit(GLOBAL.LOADING.HIDE)
@@ -21,5 +91,65 @@ export default {
 </script>
 
 <style lang="less">
+@import '~vars';
 
+@content-width: 975px;
+
+.question-knowledge-edition {
+  width: @content-width;
+
+  & > aside {
+    float: left;
+    width: 280px;
+    height: 600px;
+    padding: 10px;
+    border: 1px solid @border-color-base;
+    border-radius: 4px;
+  }
+
+  & > section {
+    padding-left: 20px;
+    overflow-x: hidden;
+  }
+
+  & > footer {
+    padding-left: 300px;
+    text-align: center;
+
+    & > button {
+      width: 80px;
+    }
+
+    & > button:first-child {
+      margin-right: 20px;
+    }
+  }
+
+  & .edition-item {
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid @border-color-base;
+    border-radius: 4px;
+
+    & > h3 {
+      margin-bottom: 15px;
+      font-size: 18px;
+
+      & > i {
+        float: right;
+        font-size: 26px;
+        color: @warning-color;
+        cursor: pointer;
+
+        &:hover {
+          color: lighten(@warning-color, 10%);
+        }
+      }
+    }
+
+    & .ivu-input-number {
+      width: 200px;
+    }
+  }
+}
 </style>
---
+++ src/views/Question/components/SideChoosen.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="v-side-tree">
+    <!-- 唯一多选模式 -->
+    <div v-if="type === 'multiple'">
+      <Button
+        type="primary" long
+      >批量选择</Button>
+    </div>
+
+    <!-- 多选单选混合模式 -->
+    <div v-else>
+      <!-- 知识点多选 -->
+      <div
+        class="multiple"
+        v-if="multiple"
+      >
+        <Button
+          type="primary"
+          @click="toggleSelectType()"
+        >切换为单选</Button>
+        <Button type="primary">确定</Button>
+      </div>
+
+      <!-- 知识点单选 -->
+      <Button
+        v-else
+        type="primary" long
+        @click="toggleSelectType()"
+      >切换为多选</Button>
+    </div>
+
+    <!-- 树形结构 -->
+    <Tree
+      :show-checkbox="multiple"
+      :data="treeData"
+    ></Tree>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: () => 'all',
+    },
+  },
+
+  data: () => ({
+    treeData: [
+      {
+        id: 1,
+        title: '高中',
+        children: [
+          {
+            id: 11,
+            title: '高一',
+            children: [
+              {
+                id: 111,
+                title: '高一数学',
+              },
+              {
+                id: 112,
+                title: '高一英语',
+              },
+              {
+                id: 113,
+                title: '高一数学',
+              },
+              {
+                id: 114,
+                title: '高一英语',
+              },
+            ],
+          },
+          {
+            id: 12,
+            title: '高二',
+            children: [
+              {
+                id: 121,
+                title: '高二物理',
+              },
+              {
+                id: 122,
+                title: '高二生物',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+
+    multiple: true,
+  }),
+
+  methods: {
+    toggleSelectType() {
+      this.multiple = !this.multiple
+    },
+  },
+}
+</script>
+
+<style lang="less">
+.v-side-tree {
+
+  & .multiple {
+
+    button {
+      width: 115px;
+
+      &:first-child {
+        margin-right: 20px;
+      }
+    }
+  }
+}
+</style>
