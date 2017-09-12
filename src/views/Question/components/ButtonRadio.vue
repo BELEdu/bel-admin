b5282b7829e896eb0e74d312a5e91b32d89ed025
huojinzhao <352782712@qq.com>
2017-09-12 15:23:19 +0800

    feat(ButtonRadio): 抽取公共组件

---
+++ src/views/Question/components/ButtonRadio.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-button-radio">
+    <Button
+      v-for="item in data" :key="item.value"
+      :size="size"
+      :type="item.value === value ? 'primary' : 'text'"
+      @click="choose(item)"
+    >
+      {{item.display_name}}
+    </Button>
+  </div>
+</template>
+
+<script>
+/**
+ * 公共组件 - 按钮式单选组件
+ *
+ * @author huojinzhao
+ */
+
+export default {
+  name: 'app-button-radio',
+
+  props: {
+    value: {},
+    data: {
+      type: Array,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: () => 'default',
+    },
+  },
+
+  methods: {
+    choose(item) {
+      if (item.value === this.value) return
+      this.$emit('input', item.value)
+      this.$emit('change', item)
+    },
+  },
+}
+</script>
+
+<style lang="less">
+.app-button-radio {
+
+  & > button {
+
+    &:not(:last-child) {
+      margin-right: 5px;
+    }
+  }
+}
+</style>
