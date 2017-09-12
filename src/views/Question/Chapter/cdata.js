a797e702d46b15056371c075602ae9cfb10fd4e4
zhoumenglin <512648622@qq.com>
2017-09-12 17:52:01 +0800

    faeat:(Chapter):章节列表

---
+++ src/views/Question/Chapter/cdata.js
@@ -0,0 +1,263 @@
+
+// 章节数据
+export default {
+  total: 22,
+  per_page: 10,
+  current_page: 1,
+  last_page: 3,
+  next_page_url: null,
+  prev_page_url: null,
+  from: 1,
+  to: 10,
+  data: [
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+    {
+      id: 1,
+      p_id: 0,
+      number: '1504234462', // 章节编号
+      display_name: '函数及其表示', // 章节名
+      grade_range_subject_id: 1, // 科目id
+      teach_material: 0, // 教材版本
+      sort: 0,
+      // 包含知识点
+      have_knowledge: [
+        '对数函数及其性质',
+        '对数与对数运算',
+        '对数与对数运算',
+      ],
+      // 关联知识点
+      relation_knowledge: [
+        '一次和二次函数',
+        '指数与指数幂的运算',
+      ],
+    },
+    {
+      id: 6,
+      p_id: 4,
+      number: '1504234462',
+      display_name: '幂函数',
+      grade_range_subject_id: 1,
+      teach_material: 0,
+      sort: 1,
+      have_knowledge: [],
+      relation_knowledge: [],
+    },
+
+  ],
+}
--- src/views/Question/Chapter/index.vue
+++ src/views/Question/Chapter/index.vue
@@ -1,5 +1,89 @@
 <template>
-  <div class="question-chapter">章节部分</div>
+  <div class="question-chapter">
+    <!-- 搜索栏 -->
+    <Form inline class="app-search-form">
+      <!-- 年段学科 -->
+      <Form-item>
+        <Select
+          v-model="query[`equal[grade_range_subject_id]`]"
+          placeholder="选择年段学科"
+          style="width:9em;"
+        >
+          <Option
+            v-for="item in subjects"
+            :value="item.value"
+            :key="item.value"
+          >
+            {{item.display_name}}
+          </Option>
+        </Select>
+      </Form-item>
+      <!-- 教材版本 -->
+      <Form-item>
+        <Select
+          v-model="query[`equal[teach_material]`]"
+          placeholder="选择教材版本"
+          style="width:9em;"
+        >
+          <Option
+            v-for="item in materials"
+            :value="item.value"
+            :key="item.value"
+          >
+            {{item.display_name}}
+          </Option>
+        </Select>
+      </Form-item>
+      <!-- 关键字检索 -->
+      <Form-item>
+        <Input placeholder="搜索关键字" v-model="likeValue">
+          <Select
+            v-model="likeKey"
+            slot="prepend"
+            style="width: 7em"
+          >
+            <Option
+              v-for="likeKey in likeKeys"
+              :key="likeKey.value"
+              :value="likeKey.value"
+            >
+              {{ likeKey.label }}
+            </Option>
+          </Select>
+        </Input>
+      </Form-item>
+      <!-- 搜索 -->
+      <Form-item>
+        <Button type="primary" icon="ios-search" @click="search">搜索</Button>
+      </Form-item>
+    </Form>
+
+    <Row class="app-content-header" type="flex" justify="space-between">
+      <Col>
+        <h2>章节列表</h2>
+      </Col>
+      <Col>
+        <Button type="primary">编辑章节</Button>
+      </Col>
+    </Row>
+
+    <!--班级管理表格-->
+    <Table
+      class="app-table"
+      :columns="columns"
+      :data="list.data"
+      border
+      @on-sort-change="sort"
+    ></Table>
+
+    <!--分页-->
+    <app-pager
+      :data="list"
+      @on-change="goTo"
+      @on-page-size-change="pageSizeChange"
+    ></app-pager>
+
+  </div>
 </template>
 
 <script>
@@ -9,10 +93,66 @@
  * @author
  */
 
+// import { mapState } from 'vuex'
+import { list } from '@/mixins'
 import { GLOBAL } from '@/store/mutationTypes'
+// import { Question } from '@/store/mutationTypes'
+import { createButton } from '@/utils'
+import cdata from './cdata'
 
 export default {
   name: 'question-chapter',
+
+  mixins: [list],
+
+  data() {
+    return {
+      likeKeys: [
+        { label: '章节名称', value: 'display_name' },
+        { label: '上级', value: 'p_id' },
+        { label: '知识点', value: 'knowledge' },
+      ],
+      likeKey: 'display_name',
+      query: {
+        'equal[grade_range_subject_id]': null,
+        'equal[teach_material]': null,
+      },
+
+      subjects: [
+        { display_name: '高中数学', value: 1 },
+        { display_name: '高中语文', value: 2 },
+        { display_name: '高中英语', value: 3 },
+        { display_name: '高中物理', value: 4 },
+      ],
+
+      materials: [
+        { display_name: '人教版', value: 1 },
+        { display_name: '沪教版', value: 2 },
+        { display_name: '黄冈版', value: 3 },
+      ],
+
+      list: cdata,
+
+      columns: [
+        { title: '章节编号', key: 'number', align: 'center' },
+        { title: '章节名称', key: 'display_name', align: 'center' },
+        { title: '上级', key: 'p_id', align: 'center' },
+        { title: '包含知识点', key: 'have_knowledge', align: 'center' },
+        { title: '关联知识点', key: 'relation_knowledge', align: 'center' },
+        {
+          title: '操作',
+          key: 10,
+          align: 'center',
+          width: 180,
+          render: createButton([
+            { text: '编辑', type: 'success', click: row => this.openManageModal(row.id) },
+          ]),
+        },
+      ],
+
+
+    }
+  },
 
   created() {
     this.$store.commit(GLOBAL.LOADING.HIDE)
