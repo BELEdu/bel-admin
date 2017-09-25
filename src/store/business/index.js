7855f6b8c3721f961cab1a86b7ba52e7f084d88b
huojinzhao <352782712@qq.com>
2017-09-25 19:20:53 +0800

    style(Business.store): 调整代码格式

--- src/store/business/index.js
+++ src/store/business/index.js
@@ -22,6 +22,7 @@ export default {
     [BUSINESS.SETURI]({ commit }, to) {
       commit(BUSINESS.SETURI, to.meta.uri)
     },
+
     // index 初始化
     [BUSINESS.PAGE.INIT]({ state, commit }, to) {
       commit(BUSINESS.SETURI, to.meta.uri)
@@ -34,6 +35,7 @@ export default {
       return Http.get(`/${state.uri}${query}`)
         .then(res => commit(BUSINESS.PAGE.INIT, { res, uri: to.meta.uri }))
     },
+
     [BUSINESS.EDIT.INIT]({ commit }, to) {
       const id = to.params.id
       const uri = to.meta.uri
@@ -43,16 +45,19 @@ export default {
       }
       return Promise.reject()
     },
+
     // 编辑页面 提交添加信息
     [BUSINESS.EDIT.CREATE]({ state }, payload) {
       // 提交成功后回到index组件
       return Http.post(`/${state.uri}`, payload)
     },
+
     // 编辑页面 提交修改信息
     [BUSINESS.EDIT.UPDATE]({ state }, { id, fdata }) {
       // 提交成功后回到index组件
       return Http.patch(`/${state.uri}/${id}`, fdata)
     },
+
     // index页面 删除信息
     [BUSINESS.EDIT.DELETE]({ state, commit }, id) {
       return Http.delete(`/${state.uri}/${id}`).then(() => commit(BUSINESS.EDIT.DELETE, id))
@@ -64,10 +69,12 @@ export default {
       // res是否要做处理
       state.buffer[uri] = res
     },
+
     // 在router的钩子中使用
     [BUSINESS.SETURI](state, uri) {
       state.uri = uri
     },
+
     [BUSINESS.EDIT.DELETE](state, id) {
       const data = state.buffer[state.uri].data
       const index = data.findIndex(item => item.id === id)
