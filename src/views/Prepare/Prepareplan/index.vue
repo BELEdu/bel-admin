b8d03b8f60dfa931c7e23d31e1d2b327793c0a67
zhoumenglin <512648622@qq.com>
2017-10-18 09:51:20 +0800

    feat(Prepare/Prepareplan):教师列表视图

--- src/views/Prepare/Prepareplan/index.vue
+++ src/views/Prepare/Prepareplan/index.vue
@@ -1,85 +1,112 @@
 <template>
-  <div class="prepareplan">
-    <!-- 上部 搜索栏 -->
-    <Form class="app-search-form">
+  <div>
+
+    <!-- 搜索 -->
+    <Form inline class="app-search-form">
+      <!-- 关键字 -->
       <Form-item>
-        <Date-picker format="yyyy/MM/dd" type="daterange" placeholder="请选择时间段..."></Date-picker>
+        <Input v-model="likeValue" placeholder="请输入关键字">
+          <Select
+            v-model="likeKey"
+            slot="prepend"
+            style="width:6em;"
+          >
+            <Option
+              v-for="likeKey in likeKeys"
+              :key="likeKey.value"
+              :value="likeKey.value"
+            >{{ likeKey.label }}</Option>
+          </Select>
+        </Input>
       </Form-item>
+
       <Form-item>
-        <Input placeholder="请输入关键字"></Input>
-      </Form-item>
-      <Form-item>
-        <Button type="primary">查询搜索</Button>
+        <Button type="primary" icon="ios-search" @click="search">搜索</Button>
       </Form-item>
     </Form>
-    <!-- 上部 搜索栏 end -->
-    <!-- 中部 列表title栏 -->
+
+    <!-- 标题 -->
     <Row class="app-content-header">
       <h2 icon="">教案管理</h2>
     </Row>
-    <!-- 中部 列表title栏 end -->
-    <!-- 下部 列表展示-->
-    <Table :data="fdata" :columns="fcol"></Table>
-    <!-- 下部 列表展示 end -->
-    <!-- 底部 分页 -->
-    <app-pager></app-pager>
-    <!-- 底部 分页 end -->
+.
+    <!--教案管理表格-->
+    <Table
+      class="app-table"
+      :columns="columns"
+      :data="list.data"
+      border
+      @on-sort-change="sort"
+    ></Table>
+
+   <!--分页-->
+    <app-pager
+      :data="list"
+      @on-change="goTo"
+      @on-page-size-change="pageSizeChange"
+    ></app-pager>
+
   </div>
 </template>
 
 <script>
 /**
- * 备课管理 - 教案管理
- * @author hjz
- * @version 2017-06-26
+ * 备课管理 - 教案管理 - 教师列表
+ * @author zhoumenglin
+ * @version 2017-10-17
  */
 
 import { GLOBAL } from '@/store/mutationTypes'
+import { list } from '@/mixins'
+import { createButton } from '@/utils'
 
 export default {
+  name: 'app-prepare-prepareplan',
+
+  mixins: [list],
 
   data() {
     return {
-      fcol: [
-        { title: '教师姓名', key: 1, align: 'center' },
-        { title: '上课科目', key: 2, align: 'center' },
-        { title: '学员个数', key: 3, align: 'center' },
-        { title: '教案总数', key: 4, align: 'center' },
-        { title: '实际上课课时', key: 5, align: 'center' },
+      likeKeys: [
+        { label: '教师姓名', value: 'display_name' },
+      ],
+      likeKey: 'display_name',
+
+      columns: [
+        { title: '教师姓名', key: 'display_name', align: 'center' },
+        { title: '班级总数量', key: 'classes_count', align: 'center', sortable: 'custom' },
+        { title: '课程总数', key: 'subject_count', align: 'center', sortable: 'custom' },
+        { title: '已完成教案', key: 'prepareplan_finish', align: 'center', sortable: 'custom' },
+        { title: '待添加教案', key: 'prepareplan_wait', align: 'center', sortable: 'custom' },
+        { title: '未完成教案', key: 'prepareplan_no', align: 'center', sortable: 'custom' },
         {
           title: '操作',
-          key: 6,
           align: 'center',
-          render: h => h(
-            'div',
-            [
-              h(
-                'Button',
-                {
-                  props: { size: 'small', type: 'text' },
-                  class: 'color-info',
-                  on: { click: this.checkInfo },
-                },
-                '查看',
-              ),
-            ],
-          ),
+          width: 180,
+          render: createButton([
+            { text: '查看', type: 'primary', click: row => this.$router.push(`/prepare/prepareplan/${row.id}`) },
+          ]),
         },
       ],
-      fdata: Array(10).fill(null).map(() => ({
-        1: '张旭',
-        2: '数学',
-        3: 5,
-        4: 5,
-        5: 5,
-      })),
+
+      // 列表假数据
+      list: {
+        data: Array(10).fill(null).map(() => ({
+          id: 10086,
+          display_name: '张老师',
+          classes_count: 3,
+          subject_count: 18,
+          prepareplan_finish: 7,
+          prepareplan_wait: 5,
+          prepareplan_no: 2,
+        })),
+      },
+
     }
   },
 
   methods: {
-    checkInfo() {
-      this.$router.push('/prepare/prepareplan/1')
-    },
+
   },
 
   created() {
@@ -89,15 +116,5 @@ export default {
 </script>
 
 <style lang="less">
-.prepareplan {
 
-  & .ivu-form-item {
-    display: inline-block;
-
-    &:last-child {
-      float: right;
-      margin: 0;
-    }
-  }
-}
 </style>
