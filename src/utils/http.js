e42dfda83963d0ba5bca9195cd0b988d52c426db
luminghuai <a-thok@outlook.com>
2017-11-06 10:49:01 +0800

    feat(http): 根据线上运行环境判断接口域名

--- src/utils/http.js
+++ src/utils/http.js
@@ -141,7 +141,7 @@ class Http {
 
 // 上线时需拼合为完整的接口地址
 Http.HOST = process.env.NODE_ENV === 'production' ?
-  'https://oa-api.caihonggou.com' : ''
+  window.location.hostname.replace(/([^.]+)\./, '$1-api.') : ''
 
 // 接口版本号
 Http.VERSION = '/v1'
