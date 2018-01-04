4cf69a99e93249397d92e44daf9bf478a50cf1a1
luminghuai <a-thok@outlook.com>
2018-01-04 14:09:17 +0800

    feat: 将教案预览相关代码同步到智慧课堂的最新版本

--- src/views/Prepare/Prepareplan/Preview.vue
+++ src/views/Prepare/Prepareplan/Preview.vue
@@ -1,36 +1,27 @@
 <template>
-  <div
-    class="no-reset prepareplan-preview"
-    :style="{
-      'background-image': `url(${assetsPreviewBackground})`,
-      'background-repeat': 'repeat-y',
-    }"
-  >
-    <div
-      class="prepareplan-preview-content"
-
-    >
+  <div class="no-reset prepareplan-preview">
+    <div class="lesson panel">
       <div
-        class="prepareplan-preview-dom"
+        class="lesson-dom -zoom"
         ref="lessonDom"
+        v-html="content"
       ></div>
       <PreviewCanvas
-        v-if="renderCanvas"
-        :width="canvasWidth"
-        :height="canvasHeight"
+        v-if="height"
+        :width="1480"
+        :height="height"
         :html="content"
-        :font-size="28"
+        :zoom="true"
       />
     </div>
   </div>
 </template>
 
 <script>
-import assetsPreviewBackground from '@/assets/preview-background.jpg'
 import PreviewCanvas from './components/PreviewCanvas'
 
 export default {
-  name: 'PrepareplanPreview',
+  name: 'Preview',
 
   components: {
     PreviewCanvas,
@@ -38,88 +29,73 @@ export default {
 
   data() {
     return {
-      assetsPreviewBackground,
-      content: '',
-      renderCanvas: false,
-      canvasWidth: 0,
-      canvasHeight: 0,
+      content: localStorage.getItem('previewPreparePlan'),
+      height: null,
     }
   },
 
-  methods: {
-    renderLesson(content, remove) {
-      this.renderCanvas = false
-
-      const { lessonDom } = this.$refs
-
-      lessonDom.innerHTML = content
-
-      this.$nextTick(() => {
-        this.canvasWidth = lessonDom.clientWidth
-        // 画布的高度计算不完全可靠，总是多给50个像素以防止内容渲染不全
-        this.canvasHeight = Math.max(lessonDom.clientHeight + 100, 800)
-
-        this.renderCanvas = true
-
-        if (remove) {
-          lessonDom.remove()
-        }
-      })
-    },
-  },
-
-  created() {
-    this.content = localStorage.getItem('previewPreparePlan')
-  },
-
   mounted() {
-    this.renderLesson(this.content, true)
+    setTimeout(() => {
+      this.height = this.$refs.lessonDom.clientHeight
+    }, 500)
   },
 }
 </script>
 
-<style lang="less">
-
+<style>
 .prepareplan-preview {
-  width: 100%;
-  padding: 90px 0;
+  background-image: url("../../../assets/preview-background.jpg");
+  background-attachment: fixed;
+  overflow: hidden;
 }
 
-.prepareplan-preview-dom {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-  width: 100%;
-  padding: 20px 60px;
-  font-family: sans-serif;
-  line-height: 1.7;
+.panel {
+  border-radius: 10px;
+  background-color: #ddd;
+  box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.4);
+}
+
+.lesson {
+  position: relative;
+  width: 1480px;
+  margin: 100px auto 40px;
+  overflow: hidden;
+}
+
+/* 教案样式 */
+.lesson-dom {
   visibility: hidden;
-  font-size: calc(28 / 16 * 1rem);
+  min-height: 800px;
+  padding: 20px 60px;
+  font-size: 16px;
 }
 
-.prepareplan-preview-dom * {
-  font-family: inherit !important;
+.lesson-dom p {
+  line-height: 24px;
+}
+
+.lesson-dom * {
+  font-family: sans-serif !important;
   font-size: inherit !important;
 }
 
-
-.prepareplan-preview-dom img {
-  mix-blend-mode: multiply;
+.lesson-dom img {
   vertical-align: middle;
-  zoom: calc(28 / 16);
 }
 
-.prepareplan-preview-content {
-  margin: 0 auto;
-  position: relative;
-  width: 1480px;
-  min-height: 800px;
-  background-color: rgb(221, 221, 221);
-  border-radius: 10px;
+.lesson-dom table, td {
+  width: auto !important;
+}
 
-  table, td {
-    width: auto !important;
-  }
+.lesson-dom.-zoom {
+  font-size: 28px;
+}
+
+.lesson-dom.-zoom p {
+  line-height: 32px;
+}
+
+.lesson-dom.-zoom img {
+  zoom: 1.75;
 }
 </style>
--- src/views/Prepare/Prepareplan/components/PreviewCanvas.vue
+++ src/views/Prepare/Prepareplan/components/PreviewCanvas.vue
@@ -1,31 +1,10 @@
 <template>
-  <div
-    class="app-canvas"
-    :style="{width: `${width}px`, height: `${height}px`}"
-  ></div>
+  <div class="draw"></div>
 </template>
 
 <script>
-/**
- * canvas画布
- * 通过inject拿到paint对象
- */
-
-/* eslint semi: 0 */
-
-// 画布模式
-const PEN = 'PEN';
-const ERASER = 'ERASER';
-
-// 画布风格高度
-const CANVAS_HEIGHT = 4000;
-
-// 画笔颜色
-export const BLACK = '#000';
-export const RED = '#f00';
-
 export default {
-  name: 'AppCanvas',
+  name: 'PreviewCanvas',
 
   props: {
     width: {
@@ -38,255 +17,225 @@ export default {
       required: true,
     },
 
-    // 富文本
-    html: {
-      type: String,
-      default: null,
-    },
+    html: String,
 
-    fontSize: {
-      type: Number,
-      default: 16,
+    drawArea: String,
+
+    zoom: {
+      type: Boolean,
+      default: false,
     },
   },
 
   data() {
     return {
       canvas: null,
-      pos: null,
-
-      htmlPaddingH: 60 - 7, // 教案内容的左右padding，画布本身左右有7个像素的空白？
-      htmlPaddingV: 20, // 教案内容的上下padding
-
-      paint: {
-        active: true,
-        mode: PEN,
-        lineWidth: 2,
-        eraserWidth: 50,
-        strokeStyle: BLACK,
-      },
-    };
+      drawing: false,
+      canvasPos: {},
+      pos: {},
+    }
   },
 
   watch: {
     width(val) {
-      this.resizeCanvas('width', val);
+      this.resizeCanvas('width', val)
     },
 
     height(val) {
-      this.resizeCanvas('height', val);
+      this.resizeCanvas('height', val)
     },
   },
 
   methods: {
-    /**
-     * 调整画布尺寸
-     * 先提取画布内容，再改变画布尺寸，然后重新把内容画回画布上
-     */
-    resizeCanvas(prop, value) {
-      const content = this.canvas.toDataURL();
-      let img = new Image();
-      img.addEventListener('load', () => {
-        this.canvas[prop] = value;
-        this.canvas.getContext('2d').drawImage(img, 0, 0);
-        img = null;
-      });
-      img.src = content;
-    },
-
-    createCanvas(height, top = 0, isPaint = true) {
-      const canvas = document.createElement('canvas');
-      canvas.width = this.width;
-      canvas.height = height;
-      canvas.style.top = `${top}px`;
-
-      if (isPaint) {
-        canvas.style.zIndex = 1;
-        this.$el.appendChild(canvas);
-        canvas.addEventListener('touchstart', this.onTouchstart);
-        canvas.addEventListener('touchmove', this.onTouchmove);
-      }
-
-      return canvas;
-    },
-
-    /**
-     * 初始化画布
-     * 教案的富文本单独渲染进一张画布，并存放在主画布的htmlCanvas属性中，需要保存到后台时再合并两种画布
-     */
-    initCanvas() {
+    init() {
+      // 教案
       if (this.html) {
-        const drafts = [];
-
-        const count = Math.floor(this.height / CANVAS_HEIGHT);
-        const lastHeight = this.height % CANVAS_HEIGHT;
-
-        for (let i = 0; i < count; i += 1) {
-          const canvas = this.createCanvas(CANVAS_HEIGHT, CANVAS_HEIGHT * i);
-          canvas.style.borderBottom = '1px dashed #ccc';
-          drafts.push(canvas);
-        }
-        const canvas = this.createCanvas(lastHeight, CANVAS_HEIGHT * count);
-        drafts.push(canvas);
-
-        this.convertHtmlToSvg(this.html)
-          .then((svg) => {
-            this.$el.appendChild(svg);
-
-            const htmlCanvas = this.createCanvas(this.height, 0, false);
-            const htmlCtx = htmlCanvas.getContext('2d');
-            htmlCtx.drawImage(svg, this.htmlPaddingH, this.htmlPaddingV);
-            drafts.push(htmlCanvas);
-
-            // 把所有画布存在全局，保存草稿时调用
-            window.drafts = drafts;
-          });
-      } else {
-        this.canvas = this.createCanvas(this.height);
-        this.$emit('init', this.canvas);
-      }
-    },
-
-    onTouchstart(event) {
-      const { paint } = this;
-
-      if (!paint.active) return;
-
-      // 必须阻止touchstart的默认行为，否则touchmove会延迟触发
-      event.preventDefault();
-
-      const ctx = event.target.getContext('2d');
-
-      // 设置画笔的基本属性
-      ctx.strokeStyle = paint.strokeStyle;
-      ctx.lineJoin = 'round';
-      if (paint.mode === PEN) {
-        ctx.globalCompositeOperation = 'source-over';
-        ctx.lineWidth = paint.lineWidth;
-      } else if (paint.mode === ERASER) {
-        ctx.globalCompositeOperation = 'destination-out';
-        ctx.lineWidth = paint.eraserWidth;
+        const img = this.convertHtmlToImage(this.html)
+        this.$el.appendChild(img)
       }
 
-      const { x, y } = this.getCanvasPosition(event.target);
-      this.pos = this.getpos(event);
-      const cx = this.pos.x - x;
-      const cy = this.pos.y - y;
-      ctx.beginPath();
-      ctx.arc(cx, cy, this.paint.lineWidth / 2, 0, 2 * Math.PI, true);
-      ctx.fill();
+      const CANVAS_HEIGHT = 4000
+      const count = Math.floor(this.height / CANVAS_HEIGHT)
+      const lastHeight = this.height % CANVAS_HEIGHT
+
+      for (let i = 0; i < count; i += 1) {
+        const canvas = this.addLayer(CANVAS_HEIGHT, CANVAS_HEIGHT * i)
+        canvas.style.borderBottom = '1px dashed #ccc'
+      }
+
+      const canvas = this.addLayer(lastHeight, CANVAS_HEIGHT * count)
+      this.canvas = canvas
+
+      // 作图区
+      if (this.drawArea) {
+        const img = this.convertHtmlToImage(this.drawArea)
+        canvas.getContext('2d').drawImage(img, 80, 150)
+      }
+
+      this.$emit('init', canvas)
     },
 
-    onTouchmove(event) {
-      const { paint } = this;
+    addLayer(height, top = 0) {
+      const canvas = document.createElement('canvas')
+      canvas.width = this.width
+      canvas.height = height
+      canvas.style.top = `${top}px`
+      this.$el.appendChild(canvas)
+      return canvas
+    },
 
-      if (!paint.active) return;
+    onPointerdown(event) {
+      event.preventDefault()
+      this.drawing = true
+      const { mode, lineWidth, eraserWidth } = this.draw
 
-      event.preventDefault();
+      const ctx = event.target.getContext('2d')
+      ctx.strokeStyle = this.strokeStyle
+      ctx.lineJoin = 'round'
+      if (mode === 'pen') {
+        ctx.globalCompositeOperation = 'source-over'
+        ctx.lineWidth = lineWidth
+      } else if (mode === 'eraser') {
+        ctx.globalCompositeOperation = 'destination-out'
+        ctx.lineWidth = eraserWidth
+      }
 
-      const ctx = event.target.getContext('2d');
-      const { x, y } = this.getCanvasPosition(event.target);
+      this.pos = this.getPoint(event)
 
-      ctx.beginPath();
-      const startX = this.pos.x - x;
-      const startY = this.pos.y - y;
-      ctx.moveTo(startX, startY);
+      this.setCanvasPosition(event.target)
+      const { x, y } = this.canvasPos
+      const cx = this.pos.x - x
+      const cy = this.pos.y - y
 
-      const nextPos = this.getpos(event);
-      const endX = nextPos.x - x;
-      const endY = nextPos.y - y;
-      ctx.lineTo(endX, endY);
-      ctx.closePath();
-      ctx.stroke();
+      ctx.beginPath()
+      ctx.arc(cx, cy, lineWidth / 2, 0, 2 * Math.PI, true)
+      ctx.fill()
+    },
 
-      this.pos = nextPos;
+    onPointermove(event) {
+      event.preventDefault()
+      if (!this.drawing) return
+
+      const { x, y } = this.canvasPos
+      const ctx = event.target.getContext('2d')
+
+      ctx.beginPath()
+      const startX = this.pos.x - x
+      const startY = this.pos.y - y
+      ctx.moveTo(startX, startY)
+
+      const nextPos = this.getPoint(event)
+
+      const endX = nextPos.x - x
+      const endY = nextPos.y - y
+      ctx.lineTo(endX, endY)
+      ctx.closePath()
+      ctx.stroke()
+
+      this.pos = nextPos
+    },
+
+    onPointerup() {
+      this.drawing = false
     },
 
     /** 获取画布的当前位置信息 */
-    getCanvasPosition(canvas) {
-      const rect = canvas.getBoundingClientRect();
-      return {
+    setCanvasPosition(canvas) {
+      const rect = canvas.getBoundingClientRect()
+      this.canvasPos = {
         x: rect.left,
         y: rect.top,
-      };
+      }
     },
 
     /** 获取当前触摸点坐标 */
     getpos(event) {
-      const [touch] = event.touches;
-      const { clientX: x, clientY: y } = touch;
-      return { x, y };
+      const [touch] = event.touches
+      const { clientX: x, clientY: y } = touch
+      return { x, y }
     },
 
-    /**
-     * 把富文本转换为svg图片以便渲染进画布
-     * 这里的样式必须和对应元素的样式保持一致
-     */
-    convertHtmlToSvg(html) {
-      const doc = document.implementation.createHTMLDocument('');
-      doc.write(html.replace(/(data-latex="[^"]+")|(<img[^>]+border: 5px solid red[^>]+>)/g, ''));
+    convertHtmlToImage(html) {
+      const doc = document.implementation.createHTMLDocument('')
+      // 过滤掉html中的data-*类型属性，这类属性有较大可能含有阻碍画布正确渲染的数据，同时过滤掉不合法的图片
+      doc.write(html.replace(/(data-[^"]+="[^"]+")|(<img[^>]+border: 5px solid red[^>]+>)/g, ''))
+      const xml = (new XMLSerializer()).serializeToString(doc).slice(15)
 
-      const xml = (new XMLSerializer())
-        .serializeToString(doc)
-        .slice(15)
-        .replace(/<head>.*<\/head>/, `<head>
-          <style>
+      const data = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="${this.width}" height="${this.height}">
+          <foreignObject width="100%" height="100%">
+            <style>
             body {
+              margin: 0;
+              padding: 20px 60px;
+            }
+            * {
               font-family: sans-serif !important;
-              font-size: ${this.fontSize}px !important;
-              line-height: 1.5;
+              font-size: ${this.zoom ? 28 : 16}px !important;
+            }
+            p {
+              line-height: ${this.zoom ? 32 : 24}px;
             }
             img {
-              zoom: ${this.fontSize / 16};
-              mix-blend-mode: multiply;
               vertical-align: middle;
+              ${this.zoom ? 'zoom: calc(28 / 16);' : ''}
             }
-            body * {
-              font-family: inherit !important;
-              font-size: inherit !important;
-              line-height: inherit !important;
-            }
-
             table, td {
               width: auto !important;
             }
-          </style>
-        </head>`);
+            </style>
+            ${xml}
+          </foreignObject>
+        </svg>
+      `
 
-      const data = `<svg xmlns="http://www.w3.org/2000/svg">
-        <foreignObject width="${this.width - (this.htmlPaddingH * 2)}" height="${this.height - this.htmlPaddingV}">
-          ${xml}
-        </foreignObject>
-      </svg>`;
-
-      return new Promise((resolve) => {
-        const img = new Image();
-        img.addEventListener('load', () => resolve(img));
-        img.src = `data:image/svg+xml;charset=utf-8,${data}`;
-      });
+      const img = new Image()
+      img.src = `data:image/svg+xml;charset=utf-8,${data}`
+      return img
     },
+
+    /**
+     * 调整画布尺寸
+     * 克隆一个画布，改变原画布尺寸，再把克隆出来的画布画回原画布上
+     */
+    resizeCanvas(prop, value) {
+      const img = new Image()
+      img.src = this.canvas.toDataURL()
+      this.canvas[prop] = value
+      this.$nextTick(() => {
+        this.canvas.getContext('2d').drawImage(img, 0, 0)
+      })
+    },
+
+    getPoint(event) {
+      const source = event.touches ? event.touches[0] : event
+      const { clientX: x, clientY: y } = source
+      return { x, y }
+    },
+
   },
 
   mounted() {
-    this.initCanvas();
+    this.init()
   },
-};
+}
 </script>
 
 <style>
-.app-canvas {
-  position: relative;
-}
-
-.app-canvas canvas {
+.draw {
   position: absolute;
   top: 0;
   left: 0;
+  z-index: 1;
 }
 
-.app-canvas img {
-  width: 100%;
-  height: 100%;
-  padding: 20px 53px;
+.draw canvas {
+  position: absolute;
+}
+
+.draw > img {
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
