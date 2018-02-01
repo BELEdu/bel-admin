94ffc5867fba95e56acec6ef2848ee994f8d72b5
huojinzhao <352782712@qq.com>
2018-02-01 20:04:18 +0800

    refactor(CommunicationEdit): 优化样式、代码优化&添加注释

--- src/views/Business/Communication/CommunicationEdit.vue
+++ src/views/Business/Communication/CommunicationEdit.vue
@@ -117,7 +117,7 @@
           icon="plus"
           @click.stop="
             addLogClick = true;
-            addLog('comm_log');
+            beforeAddLog('comm_log');
           "
         >增加沟通记录</Button>
       </Form-item>
@@ -133,7 +133,7 @@
         >
           {{item.communication_at}}
         </Col>
-        <Col span="20">
+        <Col class="commeditor--contents__detail" span="20">
           {{item.content}}
         </Col>
         <Col span="1" style="padding-left: 5px;">
@@ -232,11 +232,20 @@ export default {
       preConfig: null,
 
       dateOptions: {
+        // 沟通日期不应当超过今日
         disabledDate(date) {
           return date && date.valueOf() > Date.now()
         },
       },
     }
+  },
+
+  created() {
+    this.getPreconfig()
+    // 编辑页面初始化以及loading页面关闭
+
+    this.fetchUpdationInfo(this.$route.params.id)
+      .then(() => this.$store.commit(GLOBAL.LOADING.HIDE))
   },
 
   methods: {
@@ -257,7 +266,7 @@ export default {
 
     /* --- assistance --- */
 
-    // 表单项validator
+    // 表单沟通日期相关验证
     validateLogAt(rule, value, callback) {
       // 点击"新增沟通记录"按钮情况下，有空字段验证
       if (this.addLogClick && value === '') {
@@ -272,6 +281,7 @@ export default {
       this.addLogClick = false
     },
 
+    // 表单沟通内容相关验证
     validateLogContent(rule, value, callback) {
       const at = this.comm_log.communication_at
       // 点击"新增沟通记录"按钮情况下有空字段的验证
@@ -302,36 +312,40 @@ export default {
       this.fdata.communication_logs.splice(index, 1)
     },
 
-    // 新增log项
-    addLog(name) {
+    // 新增沟通记录前需要先验证
+    beforeAddLog(name) {
       let result = false
 
       // log验证成功后
       this.$refs[name].validate((valid) => {
-        if (valid) {
-          const communication_at = this.comm_log
-            .communication_at
-            .toJSON().slice(0, 10)
-
-          const comm_log = {
-            communication_at,
-            content: this.comm_log.content,
-          }
-
-          if (this.$route.params.id) {
-            const uri = `/communication/${this.$route.params.id}/log`
-            this.$http.post(uri, comm_log)
-          }
-
-          this.fdata.communication_logs.push(comm_log)
-          this.$refs[name].resetFields()
-        }
+        if (valid) this.addLog(name)
 
         result = valid
       })
 
       // 返回log验证结果
       return result
+    },
+
+    // 添加沟通记录
+    addLog(name) {
+      const communication_at = this.comm_log
+        .communication_at
+        .toJSON().slice(0, 10)
+
+      const comm_log = {
+        communication_at,
+        content: this.comm_log.content,
+      }
+
+      // 修改的情况下需要将记录发送给后端
+      if (this.$route.params.id) {
+        const uri = `/communication/${this.$route.params.id}/log`
+        this.$http.post(uri, comm_log)
+      }
+
+      this.fdata.communication_logs.unshift(comm_log)
+      this.$refs[name].resetFields()
     },
 
     // 编辑提交表单数据
@@ -354,7 +368,7 @@ export default {
       const content = this.comm_log.content
 
       if (communication_at !== '' || content !== '') {
-        valid = this.addLog('comm_log')
+        valid = this.beforeAddLog('comm_log')
       }
 
       return valid
@@ -376,14 +390,6 @@ export default {
         })
     },
   },
-
-  created() {
-    this.getPreconfig()
-    // 编辑页面初始化以及loading页面关闭
-
-    this.fetchUpdationInfo(this.$route.params.id)
-      .then(() => this.$store.commit(GLOBAL.LOADING.HIDE))
-  },
 }
 </script>
 
@@ -404,6 +410,10 @@ export default {
 
   &-delete {
     color: @error-color;
+  }
+
+  &__detail {
+    word-break: break-word;
   }
 }
 
