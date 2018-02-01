0be4674e652c76247ad6ed03223c8f6b6d505f96
huojinzhao <352782712@qq.com>
2018-02-01 19:19:20 +0800

    refactor(TableCheckbox): 优化样式，补充注释

--- src/views/Business/components/TableCheckbox.vue
+++ src/views/Business/components/TableCheckbox.vue
@@ -44,16 +44,19 @@ export default {
   name: 'v-table-checkbox',
 
   props: {
+    // 双向绑定的选择列表
     value: {
       type: Array,
       required: true,
     },
 
+    // 右部分checkbox列表，根据后端推送的列表数据
     list: {
       type: Array,
       required: true,
     },
 
+    // 右部分表头
     label: {
       type: String,
       required: true,
@@ -66,12 +69,15 @@ export default {
   }),
 
   watch: {
+    // 每次选值变化都会触发判断是否“全选”为true
     value: 'getAllAreasStatus',
   },
 
   methods: {
+    // “全选”选择行为
     setAllAreasStatus() {
       const nextStatus = !this.allareas
+
       if (nextStatus) {
         const list = this.list.map(item => item.id)
         this.onUpdate(list)
@@ -80,17 +86,21 @@ export default {
       }
     },
 
+    // 判断"全选"是否为true
     getAllAreasStatus() {
       let status = false
+
       if (this.value.length > 0) {
-        // 后端list数据会更新，但是value中的数据不会被更新
-        // 所以拿list到value中比对
+        // 后端数据库的list数据会更新，但是value中的数据不会被更新
+        // 所以拿list到value中对比
         status = this.list
           .every(item => this.value.includes(item.id))
       }
+
       this.allareas = status
     },
 
+    // 该组件的双向绑定输出
     onUpdate(value) {
       this.$emit('update:value', [...value])
     },
@@ -105,6 +115,11 @@ export default {
   display: flex;
   border: 1px solid @border-color-base;
 
+  dd,
+  dl {
+    margin: 0;
+  }
+
   & .left {
     flex-basis: 100px;
     flex-shrink: 0;
@@ -116,7 +131,7 @@ export default {
     }
 
     & dd {
-      text-align: center;
+      width: 100%;
 
       & .ivu-checkbox-wrapper {
         margin: 0 !important;
