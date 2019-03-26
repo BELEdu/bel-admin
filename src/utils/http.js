1c7b75fa808096bdc2f4d02a381c815ca2fe59a1
onedou <onedou@126.com>
2019-03-26 09:38:31 +0800

    update production api

--- src/utils/http.js
+++ src/utils/http.js
@@ -148,8 +148,7 @@ class Http {
 }
 
 // 上线时需拼合为完整的接口地址
-Http.HOST = process.env.NODE_ENV === 'production' ?
-  `https://${window.location.hostname.replace(/([^.]+)\./, '$1-api.')}` : ''
+Http.HOST = process.env.NODE_ENV === 'production' ? 'http://api.bel.org.cn' : ''
 
 // 接口版本号
 Http.VERSION = '/api'
